6d439fd9f524c4c135b886e60beccd1a490e9203
FioriApp <sigmarojo@gmail.com>
2023-03-06 17:40:12 +0000

    cargar archivos

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/holamundo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/holamundo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
